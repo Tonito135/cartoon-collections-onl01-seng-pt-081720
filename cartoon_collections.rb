e7e7e2295d00635e63d1e82372683179f4e82ef0
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-30 05:21:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+
+def roll_call_dwarves(name)
+  
+  
+  
 end
 
 def summon_captain_planet# code an argument here
-  # Your code here
+
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+
 end
 
 def find_the_cheese# code an argument here
-  # the array below is here to help
+ 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
