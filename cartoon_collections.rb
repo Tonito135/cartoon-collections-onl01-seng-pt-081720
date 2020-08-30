80c11423a7d43b9fdfd67cab8fc9e5006887d07c
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-30 05:36:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ short_words = ["puff", "go", "two"]
 assorted_words = ["two", "go", "industrious", "bop"]
 def long_planeteer_calls(short_words)
   short_words.any? do |short_word|
-    short_word.length < 4
+    short_word.length > 4
   end
 end
 
