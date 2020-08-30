51bebc8f6c6805b24b74fb004517b2189819260e
Anthony Hernandez <anthonyh2020@gmail.com>
2020-08-30 05:32:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,8 @@ end
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect
+  planeteer_calls.collect do |planeteer_call|
+    
 
 end
 
